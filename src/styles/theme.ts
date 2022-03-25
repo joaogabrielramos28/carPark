8b014ceeac266d047577c7e27162edf459c9ade2
joaogabrielramos28 <joaogabrielramos28@outlook.com>
2022-03-24 23:53:42 -0300

    configuring theme for the project

---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  colors: {
+    background:
+      "linear-gradient(180deg, #F4C4F3 0%, rgba(255, 253, 255, 0.03125) 96.87%, rgba(252, 103, 250, 0) 100%);",
+    title: "#383838",
+    text: "#7E7B7B",
+    shape: "#FFFFFF",
+    primary: "#AB9DF8",
+    alert: "",
+    alert_light: "#FFECED",
+    dark: "#000",
+  },
+};
